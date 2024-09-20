d106bb7ae7bf7d79abd6f26008751e080d400213
dnwwdwd <1765393959@qq.com>
2024-09-20 22:14:50 +0800

    基于 Nacos 实现动态配置黑名单

--- src/main/java/com/hjj/interviewdog/constant/RedisConstant.java
+++ src/main/java/com/hjj/interviewdog/constant/RedisConstant.java
@@ -4,6 +4,7 @@ import java.util.Date;
 
 public interface RedisConstant {
     public static String USER_SIGN_IN_REDIS_PREFIX = "interviewdog:user:signin";
+    public static String BLACK_IP_BlOOM_FILTER_KEY = "interviewdog:blackIpBloomFilterKey";
 
     static String getUserSignInRedisKey(int year, long userId) {
         return String.format("%s:%s:%s", USER_SIGN_IN_REDIS_PREFIX, userId, year);
